3245bca423bdd2b48412ce2e043b0ef89c7f37e2
GillinedUp <myrreah@gmail.com>
2018-03-24 18:49:04 +0100

    fixed find_column; refactoring

--- main.py
+++ main.py
@@ -1,7 +1,83 @@
 import sys
 import ply.lex as lex
-import scanner
 
+keywords = {
+    'if': "IF",
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
+
+tokens = [
+             'ADDASSIGN',
+             'SUBASSIGN',
+             'MULASSIGN',
+             'DIVASSIGN',
+             'DOTADD',
+             'DOTSUB',
+             'DOTDIV',
+             'DOTMUL',
+             'ID',
+             'INT',
+             'FLOAT'
+         ] + list(keywords.values())
+
+t_ADDASSIGN = r'\+='
+t_SUBASSIGN = r'-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
+t_DOTADD = r'.\+'
+t_DOTSUB = r'.-'
+t_DOTDIV = r'./'
+t_DOTMUL = r'.\*'
+
+literals = ['+', '-', '*', '/', '(', ')', '=', '{', '}', '[', ']', ',', ';', '\'', ':']
+
+t_ignore = ' \t'
+t_ignore_comment = r'\#.*'
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = keywords.get(t.value, 'ID')
+    return t
+
+
+def t_FLOAT(t):
+    r'[-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?'
+    t.value = float(t.value)
+    return t
+
+
+def t_INT(t):
+    r'-?\b[0-9]+\b'
+    t.value = int(t.value)
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def find_column(my_text, token):
+    line_start = my_text.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+
+def t_error(t):
+    print("Unrecognized token '%s' at: '%d', '%d'" % (t.value[0], t.lineno, find_column(text, t)))
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
 
 if __name__ == '__main__':
 
@@ -13,15 +89,12 @@ if __name__ == '__main__':
         sys.exit(0)
 
     text = file.read()
-    lexer = scanner.lexer
-    lexer.input(text) # Give the lexer some input
-
+    lexer.input(text)  # Give the lexer some input
 
     # Tokenize
     while True:
         tok = lexer.token()
         if not tok:
-            break    # No more input
-        column = scanner.find_column(tok)
+            break  # No more input
+        column = find_column(text, tok)
         print("(%d,%d): %s(%s)" % (tok.lineno, column, tok.type, tok.value))
-
