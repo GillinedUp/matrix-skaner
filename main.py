a4169997142e2a3260838c7bbf87760de74e40b2
GillinedUp <myrreah@gmail.com>
2018-03-25 10:18:20 +0200

    moved scanner to new file

--- main.py
+++ main.py
@@ -1,83 +1,5 @@
 import sys
-import ply.lex as lex
-
-keywords = {
-    'if': "IF",
-    'else': 'ELSE',
-    'for': 'FOR',
-    'while': 'WHILE',
-    'break': 'BREAK',
-    'continue': 'CONTINUE',
-    'return': 'RETURN',
-    'eye': 'EYE',
-    'zeros': 'ZEROS',
-    'ones': 'ONES',
-    'print': 'PRINT'
-}
-
-tokens = [
-             'ADDASSIGN',
-             'SUBASSIGN',
-             'MULASSIGN',
-             'DIVASSIGN',
-             'DOTADD',
-             'DOTSUB',
-             'DOTDIV',
-             'DOTMUL',
-             'ID',
-             'INT',
-             'FLOAT'
-         ] + list(keywords.values())
-
-t_ADDASSIGN = r'\+='
-t_SUBASSIGN = r'-='
-t_MULASSIGN = r'\*='
-t_DIVASSIGN = r'/='
-t_DOTADD = r'.\+'
-t_DOTSUB = r'.-'
-t_DOTDIV = r'./'
-t_DOTMUL = r'.\*'
-
-literals = ['+', '-', '*', '/', '(', ')', '=', '{', '}', '[', ']', ',', ';', '\'', ':']
-
-t_ignore = ' \t'
-t_ignore_comment = r'\#.*'
-
-
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = keywords.get(t.value, 'ID')
-    return t
-
-
-def t_FLOAT(t):
-    r'[-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?'
-    t.value = float(t.value)
-    return t
-
-
-def t_INT(t):
-    r'-?\b[0-9]+\b'
-    t.value = int(t.value)
-    return t
-
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-
-def find_column(my_text, token):
-    line_start = my_text.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
-
-
-def t_error(t):
-    print("Unrecognized token '%s' at: '%d', '%d'" % (t.value[0], t.lineno, find_column(text, t)))
-    t.lexer.skip(1)
-
-
-lexer = lex.lex()
+from scanner import lexer, find_column
 
 if __name__ == '__main__':
 
@@ -96,5 +18,5 @@ if __name__ == '__main__':
         tok = lexer.token()
         if not tok:
             break  # No more input
-        column = find_column(text, tok)
+        column = find_column(lexer.lexdata, tok)
         print("(%d,%d): %s(%s)" % (tok.lineno, column, tok.type, tok.value))
---
+++ scanner.py
@@ -0,0 +1,80 @@
+import ply.lex as lex
+
+keywords = {
+    'if': "IF",
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
+
+tokens = [
+             'ADDASSIGN',
+             'SUBASSIGN',
+             'MULASSIGN',
+             'DIVASSIGN',
+             'DOTADD',
+             'DOTSUB',
+             'DOTDIV',
+             'DOTMUL',
+             'ID',
+             'INT',
+             'FLOAT'
+         ] + list(keywords.values())
+
+t_ADDASSIGN = r'\+='
+t_SUBASSIGN = r'-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
+t_DOTADD = r'.\+'
+t_DOTSUB = r'.-'
+t_DOTDIV = r'./'
+t_DOTMUL = r'.\*'
+
+literals = ['+', '-', '*', '/', '(', ')', '=', '{', '}', '[', ']', ',', ';', '\'', ':']
+
+t_ignore = ' \t'
+t_ignore_comment = r'\#.*'
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = keywords.get(t.value, 'ID')
+    return t
+
+
+def t_FLOAT(t):
+    r'[-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?'
+    t.value = float(t.value)
+    return t
+
+
+def t_INT(t):
+    r'-?\b[0-9]+\b'
+    t.value = int(t.value)
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def find_column(my_text, token):
+    line_start = my_text.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+
+def t_error(t):
+    print("Unrecognized token '%s' at: '%d', '%d'"
+          % (t.value[0], t.lineno, find_column(lexer.lexdata, t)))
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
